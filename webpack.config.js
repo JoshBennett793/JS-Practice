3c7380ff90d76e769bd5b5e96872dbcd9ea9394c
JoshBennett <joshbennett793@yahoo.com>
2023-03-03 22:14:30 -0600

    Style and build drop down functionality for menu

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
+    menu: './src/menu.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+			directory: path.join(__dirname, './dist'),
+		},
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'JavaScript Practice',
+			template: "./src/index.html",
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
