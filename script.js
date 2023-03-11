60de1e80ba3ef1da49b89195df38b20bed211251
Josh Bennett <joshbennett793@yahoo.com>
2023-03-10 22:47:19 -0600

    Add email input, style it, and validate with JS

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-const container = document.querySelector('#container');
+const form = document.querySelector('form');
+const email = document.getElementById('mail');
+const emailError = document.querySelector('#mail + span.error');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
-
-const paragraph = document.createElement("p");
-paragraph.classList.add('red');
-paragraph.textContent = 'Hey I\'m red!';
-paragraph.style.color = 'red';
-
-const header = document.createElement('h3');
-header.classList.add('blue');
-header.textContent = 'I\'m a blue h3!';
-header.style.color = 'blue';
-
-const secondDiv = document.createElement('div');
-secondDiv.classList.add('box');
-let secondDivH1 = document.createElement('h1');
-secondDivH1.textContent = 'I\'m in a div';
-secondDiv.appendChild(secondDivH1);
-let secondDivPara = document.createElement('p');
-secondDivPara.textContent = 'ME TOO!';
-secondDiv.appendChild(secondDivPara);
-secondDiv.style.cssText = 'background: pink; border: 5px solid black;'
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-	button.addEventListener('click', () => {
-		alert(button.id);
-	});
+email.addEventListener('input', (e) => {
+  if (email.validity.valid) {
+    emailError.textContent = '';
+    emailError.className = 'error';
+  } else {
+    showError();
+  }
 });
 
+form.addEventListener('submit', (e) => {
+	if (!email.validity.valid) {
+		showError();
+		e.preventDefault();
+	}
+});
 
-
-container.appendChild(content);
-container.appendChild(paragraph);
-container.appendChild(header);
-container.appendChild(secondDiv);
+const showError = () => {
+	if (email.validity.valueMissing) {
+		emailError.textContent = 'You need to enter an email address.';
+	} else if (email.validity.typeMismatch) {
+		emailError.textContent = 'Entered value needs to be an email address.';
+	} else if (email.validity.tooShort) {
+		emailError.textContent = `Email should be at least ${email.minLength} characters; you entered
+		${email.value.length}.`;
+	}
+	emailError.className = 'error active';
+}
